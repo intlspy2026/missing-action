71bb1855bda84d22a8ed9f1671e411d4d60fea09
intlspy2026 <bdtsrbz9hk@privaterelay.appleid.com>
2026-05-31 01:26:58 +0530

    fix: restore 'including but not limited' catch-all markers

--- src/agents/external_agent/external_agent_graph.py
+++ src/agents/external_agent/external_agent_graph.py
@@ -137,6 +137,8 @@ _HOOK_REGEXES = {
 
 _CATCH_ALL_MARKERS = [
     "any other document",
+    "including but not limited to",
+    "including but not limited",
     "other documents",
     "other supporting",
 ]
